ab4fd0cb8a994c36a3d1030bfcf37f5078626b77
ggg <ggg@Aitalys-MacBook-Air.local>
2024-05-31 14:54:07 +0600

    HomeWork 5

--- app/src/main/java/com/example/noteapp/ui/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/noteapp/ui/adapter/NoteAdapter.kt
@@ -2,6 +2,7 @@ package com.example.noteapp.ui.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -9,36 +10,46 @@ import com.example.noteapp.data.models.NoteModel
 import com.example.noteapp.databinding.ItemNoteBinding
 import com.example.noteapp.interfaces.OnClickItem
 
-class NoteAdapter(private val onLongClick: OnClickItem, private val onClick: OnClickItem):ListAdapter<NoteModel , NoteAdapter.ViewHolder>(DiffUtilCallBack()) {
-    class ViewHolder(private val binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: NoteModel) {
-            binding.itemTitle.text = item.title
-            binding.itemDescription.text = item.description
-            binding.itemTimeMonth.text = item.date
-            binding.itemTimeHours.text = item.time
-        }
-    }
+
+class NoteAdapter(private val onLongClick: OnClickItem, private val onClick: OnClickItem) :
+    ListAdapter<NoteModel, NoteAdapter.ViewHolder>(DiffUtilCallBack()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding=ItemNoteBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        val binding = ItemNoteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(getItem(position))
 
-        holder.itemView.setOnLongClickListener{
+        holder.itemView.setOnLongClickListener {
             onLongClick.onLongClick(getItem(position))
             true
         }
 
-        holder.itemView.setOnClickListener{
+        holder.itemView.setOnClickListener {
             onClick.onClick(getItem(position))
+        }
+
+    }
+
+    class ViewHolder(private val binding: ItemNoteBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: NoteModel) {
+            binding.itemTitle.text = item.title
+            binding.itemDescription.text = item.description
+            binding.itemTimeMonth.text = item.date
+            binding.itemTimeHours.text = item.time
+
+
+            val context = binding.root.context
+            val drawable = ContextCompat.getDrawable(context, item.color.toInt())
+            binding.cvItem.background = drawable
         }
     }
 }
 
-class DiffUtilCallBack: DiffUtil.ItemCallback<NoteModel>(){
+
+class DiffUtilCallBack : DiffUtil.ItemCallback<NoteModel>() {
     override fun areItemsTheSame(oldItem: NoteModel, newItem: NoteModel): Boolean {
         return oldItem == newItem
     }
--- app/src/main/java/com/example/noteapp/ui/fragments/note/NoteFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/fragments/note/NoteFragment.kt
@@ -1,43 +1,37 @@
 package com.example.noteapp.ui.fragments.note
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.marginEnd
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.noteapp.App
 import com.example.noteapp.R
-import com.example.noteapp.data.extension.getBackStackData
 import com.example.noteapp.data.models.NoteModel
 import com.example.noteapp.databinding.FragmentNoteBinding
 import com.example.noteapp.interfaces.OnClickItem
 import com.example.noteapp.ui.adapter.NoteAdapter
-import com.example.noteapp.utils.PreferenceHelper
 
 
-class NoteFragment : Fragment(),OnClickItem {
+class NoteFragment : Fragment(), OnClickItem {
 
     private lateinit var binding: FragmentNoteBinding
 
-    private val noteAdapter= NoteAdapter(this,this)
+    private val noteAdapter = NoteAdapter(this, this)
 
     private var isGridLayout: Boolean = false
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentNoteBinding.inflate(inflater,container,false)
+        binding = FragmentNoteBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
     }
@@ -45,15 +39,15 @@ class NoteFragment : Fragment(),OnClickItem {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        init()
+        initialization()
         setupListeners()
         getData()
     }
 
-    private fun init() {
+    private fun initialization() {
         binding.rvNote.apply {
             layoutManager = LinearLayoutManager(requireContext())
-            adapter=noteAdapter
+            adapter = noteAdapter
         }
     }
 
@@ -63,37 +57,49 @@ class NoteFragment : Fragment(),OnClickItem {
         }
 
         btnChangeLayout.setOnClickListener {
-            isGridLayout = !isGridLayout
-            val layoutManager = if(isGridLayout){
-                GridLayoutManager(requireContext(),2)
-            }else{
-                LinearLayoutManager(requireContext())
-            }
-            binding.rvNote.layoutManager = layoutManager
+            toggleLayout()
         }
+        btnChangeLayoutToLinear.setOnClickListener {
+            toggleLayout()
+        }
+
 
     }
 
-    private fun getData(){
-        App().getInstance()?.noteDao()?.getAll()?.observe(viewLifecycleOwner){
+    private fun getData() {
+        App().getInstance()?.noteDao()?.getAll()?.observe(viewLifecycleOwner) {
             noteAdapter.submitList(it)
         }
     }
 
     override fun onLongClick(noteModel: NoteModel) {
         val builder = AlertDialog.Builder(requireContext())
-        with(builder){
+        with(builder) {
             setTitle("Вы точно хотите удалить?")
-            setPositiveButton("Да"){dialog, which->
+            setPositiveButton("Да") { dialog, which ->
                 App().getInstance()?.noteDao()?.deleteNote(noteModel)
             }
-            setNegativeButton("Нет"){dialog, which->
+            setNegativeButton("Нет") { dialog, which ->
                 dialog.cancel()
             }
             show()
         }
 
         builder.create()
+    }
+
+    private fun toggleLayout() {
+        isGridLayout = !isGridLayout
+        val layoutManager = if (isGridLayout) {
+            binding.btnChangeLayout.visibility = View.GONE
+            binding.btnChangeLayoutToLinear.visibility = View.VISIBLE
+            GridLayoutManager(requireContext(), 2)
+        } else {
+            binding.btnChangeLayoutToLinear.visibility = View.GONE
+            binding.btnChangeLayout.visibility = View.VISIBLE
+            LinearLayoutManager(requireContext())
+        }
+        binding.rvNote.layoutManager = layoutManager
     }
 
     override fun onClick(noteModel: NoteModel) {
